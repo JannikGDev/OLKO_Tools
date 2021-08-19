f6a10febd08b2ac391ab3d56b8d6d26667c0c415
JannikG <sodacookiedev@gmail.com>
2021-08-19 20:36:33 +0200

    Update

---
+++ Runtime/Systems/UISystem/ColorPalette.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="ColorPalette", menuName ="ColorPalette")]
+public class ColorPalette : ScriptableObject
+{
+    public Color Main1Light;
+    public Color Main1Dark;
+    public Color Main2Light;
+    public Color Main2Dark;
+    public Color Main3Light;
+    public Color Main3Dark;
+    public Color Main4Light;
+    public Color Main4Dark;
+    public Color Main5Light;
+    public Color Main5Dark;
+
+    public Color Side1Light;
+    public Color Side1Dark;
+    public Color Side2Light;
+    public Color Side2Dark;
+    public Color Side3Light;
+    public Color Side3Dark;
+    public Color Side4Light;
+    public Color Side4Dark;
+    public Color Side5Light;
+    public Color Side5Dark;
+}
---
+++ Runtime/Systems/UISystem/ColorTest.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class ColorTest : MonoBehaviour
+{
+
+}
